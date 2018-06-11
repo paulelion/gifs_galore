1c6a8fcfab1ea523d61534790378525cafc73f4e
Paul Elion <paul.j.elion@gmail.com>
2018-06-11 11:45:28 -0400

    add randomize button and functionality

--- js/main.js
+++ js/main.js
@@ -3,17 +3,34 @@ function getGifs(){
 
 	var input = $("#searchtext").val()
 
-var xhr = $.get("http://api.giphy.com/v1/gifs/search?q="+input+"&api_key=7aFQfk9FpPpZjgmPUyJ0PLb79Cej5iNw&limit=30");
+	var xhr = $.get("http://api.giphy.com/v1/gifs/search?q="+input+"&api_key=7aFQfk9FpPpZjgmPUyJ0PLb79Cej5iNw&limit=30");
 
-xhr.done(function(response) { 
-	console.log("success got data", response);
+	xhr.done(function(response) { 
+		console.log("success got data", response);
 
-	var gifs = response.data
+		var gifs = response.data
 
-	for (i in gifs) 
-	{
-	$('.inner').append("<img src='"+gifs[i].images.original.url+"'/>")
-	}
-		});
+		for (i in gifs) 
+		{
+		$('.inner').append("<img src='"+gifs[i].images.original.url+"'/>")
+		}
+	});
+
+}
+
+
+function getRandomGifs(){
+
+	var input = $("#searchtext").val()
+
+	var xhr = $.get("http://api.giphy.com/v1/gifs/random?&api_key=7aFQfk9FpPpZjgmPUyJ0PLb79Cej5iNw");
+
+	xhr.done(function(response) { 
+		console.log("success got data", response);
+
+		var gifs = response.data
+		$('.inner').append("<img src='"+gifs.image_url+"'/>")
+	
+	});
 
 }
